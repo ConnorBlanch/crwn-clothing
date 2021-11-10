ed929b2fdaa983c4c9fd6daecf334e9795d0639c
Connor Blanch <connorblanch@gmail.com>
2021-11-10 14:25:53 -0500

    Updated with shop page and completed collection-preview and collextion-item components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
 import './App.css';
-
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 function App() {
   return (
     <div> 
-      <HomePage /> 
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route exact path='/shop' element={<ShopPage />} />
+      </Routes>
     </div>
   );
 }
